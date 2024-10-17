f15fd83ae31e73892444c886221517b68275068d
Martino Bonetto <bonettogmartino@gmail.com>
2024-10-17 11:35:02 -0300

    Se agrego responsive

---
+++ menu.js
@@ -0,0 +1,15 @@
+const abrirMenu = document.querySelector("#abrir-menu");
+const cerrarMenu = document.querySelector("#cerrar-menu");
+const aside = document.querySelector("aside");
+
+abrirMenu.addEventListener("click", () => {
+    aside.classList.add("aside-visible");
+})
+
+cerrarMenu.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+})
+
+botonescategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
